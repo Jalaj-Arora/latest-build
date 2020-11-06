d26379da6a6525cafe66ab27fc87a0f88d474ff7
Jalaj Arora <jalaj.arora@harbingergroup.com>
2020-11-07 04:18:46 +0530

    Initial

---
+++ build/precache-manifest.0f0db98b859170da1a90197b1d196b29.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3a645611486b4252a8d6d9abafc6b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a7f116409641f30e1ee",
+    "url": "/static/css/2.cbe6aa73.chunk.css"
+  },
+  {
+    "revision": "2430f6e6a6825fb3ac3e",
+    "url": "/static/css/main.a426093d.chunk.css"
+  },
+  {
+    "revision": "3a7f116409641f30e1ee",
+    "url": "/static/js/2.be74136f.chunk.js"
+  },
+  {
+    "revision": "c6f5e14124ede098fbfafa47cf88483d",
+    "url": "/static/js/2.be74136f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2430f6e6a6825fb3ac3e",
+    "url": "/static/js/main.a73b323b.chunk.js"
+  },
+  {
+    "revision": "f4de7d0a9c256b025936",
+    "url": "/static/js/runtime-main.47b6b39e.js"
+  },
+  {
+    "revision": "b628c8647a496f5905ef27cc3c320bcd",
+    "url": "/static/media/BroadcastMsg.b628c864.svg"
+  },
+  {
+    "revision": "1b17639f25d178fcfb6485a7206979ad",
+    "url": "/static/media/Help.1b17639f.svg"
+  },
+  {
+    "revision": "8e399cf399cd9ec9601c9dd9164099c3",
+    "url": "/static/media/HotLine.8e399cf3.svg"
+  },
+  {
+    "revision": "a71a0872bf184e869e0a889cde97dddb",
+    "url": "/static/media/Info.a71a0872.svg"
+  },
+  {
+    "revision": "b979556aa539d25e6ff55dad8782e4de",
+    "url": "/static/media/ManageEmp.b979556a.svg"
+  },
+  {
+    "revision": "19ed48f2083e08bebbccd702ed6134cc",
+    "url": "/static/media/OpenLine.19ed48f2.svg"
+  },
+  {
+    "revision": "797dc4b6ba0ca6dca47afb769396a86a",
+    "url": "/static/media/Polls.797dc4b6.svg"
+  },
+  {
+    "revision": "042c916335f0c1a89c85997f892c900d",
+    "url": "/static/media/amcc-vector.042c9163.svg"
+  },
+  {
+    "revision": "c7b270bafae1db29c89d505e5ff62da5",
+    "url": "/static/media/anonymoose-logo.c7b270ba.svg"
+  },
+  {
+    "revision": "81f1e06e8775f94806dbd800af46f3ee",
+    "url": "/static/media/anonymoose.81f1e06e.eot"
+  },
+  {
+    "revision": "9fdadd8daab4b2ae0f23d7528d57db92",
+    "url": "/static/media/anonymoose.9fdadd8d.ttf"
+  },
+  {
+    "revision": "fc0f2810637962b6a07781aa8e575622",
+    "url": "/static/media/anonymoose.fc0f2810.svg"
+  },
+  {
+    "revision": "ff6971421ff4dcf580f87d0b2283d603",
+    "url": "/static/media/anonymoose.ff697142.woff"
+  },
+  {
+    "revision": "7fcce0f6c182f0105a8bede093dcc357",
+    "url": "/static/media/create-hotline.7fcce0f6.svg"
+  },
+  {
+    "revision": "aa302437f5a4f04ab34a019c6e5e3ed4",
+    "url": "/static/media/dots-icon.aa302437.svg"
+  },
+  {
+    "revision": "2d8e2c25eac5fac10c6d101d53087f98",
+    "url": "/static/media/expand-more.2d8e2c25.svg"
+  },
+  {
+    "revision": "c7b270bafae1db29c89d505e5ff62da5",
+    "url": "/static/media/logo.c7b270ba.svg"
+  },
+  {
+    "revision": "5fa26e5e0eef02b428b5fcec33b565fb",
+    "url": "/static/media/pencil.5fa26e5e.svg"
+  }
+]);
